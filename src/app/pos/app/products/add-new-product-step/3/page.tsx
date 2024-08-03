a621d2c5adf738590e500d299640513f2c0b7a4a
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-08-03 20:57:08 +0630

    page 3 changes

--- src/app/pos/app/products/add-new-product-step/3/page.tsx
+++ src/app/pos/app/products/add-new-product-step/3/page.tsx
@@ -1,134 +1,126 @@
-// "use client";
-
-// import { useProductProvider } from "@/app/pos/app/products/Provider/ProductProvider";
-// import { AddProductControlBar } from "@/components/pos/products";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import Image from "next/image";
-// import { useState } from "react";
-// import { FilePond } from "react-filepond";
-// import { useForm } from "react-hook-form";
-// import { z } from "zod";
+"use client";
+import { useProductProvider } from "@/app/pos/app/products/Provider/ProductProvider";
+import { AddProductControlBar } from "@/components/pos/products";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useState } from "react";
+import { FilePond } from "react-filepond";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const AddProductPageThree = () => {
-  // const {
-  //   navigateForward,
-  //   navigateBackward,
-  //   addProductFormData,
-  //   setAddProductFormData,
-  // } = useProductProvider();
+  const {
+    navigateForward,
+    navigateBackward,
+    addProductFormData,
+    setAddProductFormData,
+  } = useProductProvider();
 
-  // // to display
-  // const [images, setImages] = useState<any[]>([]);
 
-  // const handleFileUpdate = (fileItems: any) => {
-  //   const validFiles = fileItems.map((fileItem: any) => fileItem.file);
-  //   setImages(validFiles);
-  // };
+  // to display
+  const [images, setImages] = useState<File[]>([]);
 
-  // const validImageTypes = ["image/jpeg", "image/png", "image/jpg"];
-  // const MAX_FILE_SIZE = 20000000;
+  const handleFileUpdate = (fileItems: any) => {
+    const validFiles = fileItems.map((fileItem: any) => fileItem.file);
 
-  // const schema = z.object({
-  //   image: z
-  //     .array(
-  //       z.object({
-  //         file: z
-  //           .any()
-  //           .refine(
-  //             (file) =>
-  //               typeof File !== "undefined" &&
-  //               file instanceof File &&
-  //               validImageTypes.includes(file.type),
-  //             {
-  //               message: "Invalid file type",
-  //             }
-  //           )
-  //           .refine(
-  //             (file) => typeof File !== "undefined" && file.size <= MAX_FILE_SIZE,
-  //             {
-  //               message: "File size too large",
-  //             }
-  //           ),
-  //       })
-  //     )
-  //     .min(1, { message: "At least one image is required" })
-  //     .max(5, { message: "No more than 5 images are allowed" }),
-  // });
+    setImages(validFiles);
+  };
 
-  // type FormData = z.infer<typeof schema>;
+  const validImageTypes = ["image/jpeg", "image/png", "image/jpg"];
+  const MAX_FILE_SIZE = 20000000;
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   setValue,
-  //   formState: { errors },
-  // } = useForm<FormData>({
-  //   resolver: zodResolver(schema),
-  //   defaultValues: {
-  //     image: addProductFormData.image || [],
-  //   },
-  // });
 
-  // const onSubmit = (data: FormData) => {
-  //   // setAddProductFormData({
-  //   //   ...addProductFormData,
-  //   //   ...data,
-  //   //   name: ""
-  //   // });
-  //   navigateForward(4);
-  // };
+  const schema = z.object({
+    image: z
+      .array(
+        z.object({
+          file: z
+            .instanceof(File)
+            .refine((file) => validImageTypes.includes(file.type), {
+              message: "Invalid file type",
+            })
+            .refine((file) => file.size <= MAX_FILE_SIZE, {
+              message: "File size too large",
+            }),
+        })
+      )
+      .min(1, { message: "At least one image is required" })
+      .max(5, { message: "No more than 5 images are allowed" }),
+  });
 
-  // return (
-  //   <div className="space-y-4 ">
-  //     <AddProductControlBar
-  //       goBackward={navigateBackward}
-  //       goForward={handleSubmit(onSubmit)}
-  //     />
+  type FormData = z.infer<typeof schema>;
 
-  //     <div className="w-1/2">
-  //       <FilePond
-  //         className="!bg-white !rounded-md"
-  //         allowMultiple={true}
-  //         onupdatefiles={(fileItems: any) => {
-  //           handleFileUpdate(fileItems);
-  //           const validFiles = fileItems.map((fileItem: any) => ({
-  //             file:
-  //               typeof File !== "undefined" && fileItem.file instanceof File
-  //                 ? fileItem.file
-  //                 : (fileItem.file.file as File),
-  //           }));
-  //           setValue("image", validFiles, { shouldValidate: true });
-  //           setAddProductFormData({
-  //             ...addProductFormData,
-  //             image: validFiles,
-  //           });
-  //         }}
-  //         allowDrop={true}
-  //         maxFiles={5}
-  //         server={null}
-  //         instantUpload={false}
-  //       />
-  //       {errors.image && (
-  //         <p className="text-sm text-red-500">{errors.image.message}</p>
-  //       )}
-  //     </div>
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      image: addProductFormData.image || [],
+    },
+  });
 
-  //     <div className=" w-full overflow-auto">
-  //       <div className=" flex gap-3 justify-start items-center">
-  //         {images.map((file, index) => (
-  //           <Image
-  //             key={index}
-  //             className=" object-cover"
-  //             src={URL.createObjectURL(file)}
-  //             alt=""
-  //             width={400}
-  //             height={400}
-  //           />
-  //         ))}
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  const onSubmit = (data: FormData) => {
+    setAddProductFormData({
+      ...addProductFormData,
+      ...data,
+    });
+    navigateForward(4);
+  };
+
+  return (
+    <div className="space-y-4 ">
+      <AddProductControlBar
+        goBackward={navigateBackward}
+        goForward={handleSubmit(onSubmit)}
+      />
+
+      <div className="w-1/2">
+        <FilePond
+          className="!bg-white !rounded-md"
+          allowMultiple={true}
+          onupdatefiles={(fileItems: any) => {
+            handleFileUpdate(fileItems);
+            const validFiles = fileItems.map((fileItem: any) => ({
+              file:
+                fileItem.file instanceof File
+                  ? fileItem.file
+                  : (fileItem.file.file as File),
+            }));
+            setValue("image", validFiles, { shouldValidate: true });
+            setAddProductFormData({
+              ...addProductFormData,
+              image: validFiles,
+            });
+          }}
+          allowDrop={true}
+          maxFiles={5}
+          server={null}
+          instantUpload={false}
+        />
+        {errors.image && (
+          <p className="text-sm text-red-500">{errors.image.message}</p>
+        )}
+      </div>
+
+      <div className=" w-full overflow-auto">
+        <div className=" flex gap-3 justify-start items-center">
+          {images.map((file, index) => (
+            <Image
+              key={index}
+              className=" object-cover"
+              src={URL.createObjectURL(file)}
+              alt=""
+              width={400}
+              height={400}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddProductPageThree;
