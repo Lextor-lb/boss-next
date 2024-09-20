2a242aa937a33e111d009dde3f980dd3c0b930cb
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-20 15:22:50 +0630

    log in for token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt, { JwtPayload } from 'jsonwebtoken'; // Import jwt for decoding tokens
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
@@ -16,7 +17,29 @@ export async function middleware(req: NextRequest) {
   }
 
   // For paths that start with "/pos/" (excluding /pos/login), check for the presence of the token
-  const token = req.cookies.get("accessToken");
+
+  const token = req.cookies.get("accessToken")?.value;
+
+
+    try {
+        if (typeof token === 'string') 
+          {
+
+            const decodedToken = jwt.verify(token, "zjP9h6ZI5LoSKCRj");
+
+            console.log(`decodedToken ${decodedToken}`);
+      
+            if (typeof decodedToken === 'object' && (decodedToken as JwtPayload).role === "ECOMUSER") {
+              const loginUrl = new URL("/pos/login", FRONTEND_URL);
+              return NextResponse.redirect(loginUrl);
+            }
+
+            
+        }
+    } catch (error) {
+        const loginUrl = new URL("/pos/login", FRONTEND_URL);
+        return NextResponse.redirect(loginUrl);
+    }
 
   if (!token) {
     // Redirect to login page if no token is present
